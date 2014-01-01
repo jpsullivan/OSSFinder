6d105cfd5cc71285b425ea0f2c8ff63162c8f975
Josh Sullivan <jaysche.sullivan@gmail.com>
2014-01-01 18:50:48 -0500

    Added in the NewProject view model

---
+++ OSSFinder/Models/ViewModels/NewProjectViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using OSSFinder.Infrastructure.Attributes;
+
+namespace OSSFinder.Models.ViewModels
+{
+    public class NewProjectViewModel
+    {
+
+        public NewProjectViewModel() { }
+
+
+    }
+
+    public class NewProjectStep1
+    {
+        [Required]
+        [Display(Name = "Repository URL")]
+        [Hint("Enter in the repository URL for this project")]
+        public Uri RepositoryUrl { get; set; }
+
+        [Required]
+        [Display(Name = "Project Name")]
+        public string ProjectName { get; set; }
+
+        [Display(Name = "Project Description")]
+        public string ProjectDesscription { get; set; }
+
+        public NewProjectStep1() { }
+    }
+}
